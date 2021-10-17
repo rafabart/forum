99ebc4c524012deaea78d95f587adc759239451e
Rafael <rafamola@gmail.com>
2021-10-17 15:23:22 -0300

    First commit

---
+++ src/main/kotlin/com/forum/model/StatusTopico.kt
@@ -0,0 +1,9 @@
+package com.forum.model
+
+enum class StatusTopico {
+
+    NAO_RESPONDIDO,
+    NAO_SOLUCIONADO,
+    SOLUCIONADO,
+    FECHADO
+}
---
+++ src/main/kotlin/com/forum/model/Usuario.kt
@@ -0,0 +1,8 @@
+package com.forum.model
+
+data class Usuario(
+
+    val id: Long? = null,
+    val nome: String,
+    val email: String
+)
